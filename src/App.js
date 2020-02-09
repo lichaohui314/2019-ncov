ad1921d02851984a654a77bfc9c43f700d19895d
a316760006 <316760006@qq.com>
2020-02-09 16:56:01 +0800

    '统计'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+const Map = React.lazy(() => import('./Map'))
+
+
+function Header ({ province }) {
+  return (
+    <header>
+      <div className="bg"></div>
+      <h1>
+        <small>新型冠状病毒</small>
+        <br />
+        疫情实时动态 · { province ? province.name : '省市地图' }
+      </h1>
+      <i>By 全栈成长之路</i>
+    </header>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Map></Map>
     </div>
   );
 }
